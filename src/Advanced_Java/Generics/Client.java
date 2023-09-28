908a95f9500a6ecbac1c7697323863b11c863783
ujjwal <ujjwal.jindal3@gmail.com>
2023-09-28 16:26:48 +0530

    generics

---
+++ src/Advanced_Java/Generics/Client.java
@@ -0,0 +1,42 @@
+package Advanced_Java.Generics;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) {
+
+//        HashMap<Integer, String> hm = new HashMap<>();
+
+        //type erasure
+//        System.out.println(hm.getClass().getName());
+
+
+//        hm.put(1, "Ujj");
+//        hm.put("jin", "ujj");
+
+        HashMap hm = new HashMap();
+        hm.put(1, "Ujj");
+        hm.put("ujj", 1);
+
+        for (var v : hm.keySet()) {
+            int i = (Integer) v;
+            System.out.println(i);
+        }
+
+//        List
+
+        Utilites utilites = new Utilites();
+
+        // show type from object to string here
+        utilites.doSomething("abc", "def");
+
+        // not allowed as returns type of first which is string
+//        int val = utilites.getFirst("abc", "def");
+
+        int val = utilites.getFirst(1, "abc");
+
+        // allowed as type as method level not at class level
+        utilites.getFirst("abc", 1);
+    }
+}
---
+++ src/Advanced_Java/Generics/Utilites.java
@@ -0,0 +1,24 @@
+package Advanced_Java.Generics;
+
+//public class Utilites<K, V> {
+//
+//    // static methods are not associated to class object so they cant refer to this type K or V
+//    public static void doSomething(K key, V value) {
+//        System.out.println(key);
+//        System.out.println(value);
+//    }
+//}
+
+
+public class Utilites {
+
+    // these parameters are needed only for this method and not the entire class level
+    public <K, V> void doSomething(K key, V value) {
+        System.out.println(key);
+        System.out.println(value);
+    }
+
+    public <K, V> K getFirst(K key, V value) {
+        return key;
+    }
+}
